a0990d50014ce4b1f43cf5fa5b6645c73327c8ed
Rich1997 <32636319+Rich1997@users.noreply.github.com>
2023-01-20 23:22:08 +0530

    1 file modified, 1 added

---
+++ src/config/context/ThemeContext.tsx
@@ -0,0 +1,29 @@
+import { createContext, useContext, useState } from 'react';
+
+export enum Theme {
+    dark = 'dark',
+    light = '',
+}
+
+export type ThemeContextType = {
+    theme: Theme;
+    setTheme: (Theme: Theme) => void;
+};
+
+export const ThemeContext = createContext<ThemeContextType>({
+    theme: Theme.dark,
+    setTheme: (theme) => console.warn('no theme provider'),
+});
+
+export const useTheme = () => useContext(ThemeContext);
+
+export const ThemeProvider = (props: { children: React.ReactNode }) => {
+    const [darkMode, setDarkMode] = useState(Theme.dark);
+    return (
+        <ThemeContext.Provider
+            value={{ theme: darkMode, setTheme: setDarkMode }}
+        >
+            {props.children}
+        </ThemeContext.Provider>
+    );
+};
