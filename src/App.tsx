9b46ead9a00342403f66823106a44a4751cbf187
Rich1997 <32636319+Rich1997@users.noreply.github.com>
2023-01-21 21:47:00 +0530

    1 file modified

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,14 @@ import { useTheme } from './config/context/ThemeContext';
 function App() {
     const { theme } = useTheme();
     return (
-        <div className={`App ${theme} w-full min-h-screen h-full}`}>
-            <Navbar />
-            <div className="default-bg default-text text-4xl p-12">
-                <Sampler font="Rubik" />
-                <Sampler font="Inter" />
-                <Sampler font="Rubik" />
+        <div className={`${theme}`}>
+            <div className="default-bg default-text w-full min-h-screen h-full">
+                <Navbar />
+                <div className="default-bg default-text text-4xl p-12">
+                    <Sampler font="Rubik" />
+                    <Sampler font="Inter" />
+                    <Sampler font="Rubik" />
+                </div>
             </div>
         </div>
     );
