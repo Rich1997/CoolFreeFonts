0fcfb0b19af6ccd58633b825cf561c3e29a4a8c6
Rich1997 <32636319+Rich1997@users.noreply.github.com>
2023-03-02 00:35:45 +0530

    3 files modified, 1 deleted

--- env.ts
+++
@@ -1,3 +0,0 @@
-export const isGitHubPages =
-    process.env.NODE_ENV === 'production' &&
-    window.location.hostname === 'rich1997.github.io';
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(() => {
-    return {
-        plugins: [react()],
-        base: process.env.VITE_GITHUB_PAGES ? '/CoolFreeFonts/' : '/',
-    };
+export default defineConfig({
+    plugins: [react()],
+    base: '/',
 });
