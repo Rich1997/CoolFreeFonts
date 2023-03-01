64f2231e0163ba1fa50ed8d02b91aa8e9f4fdcae
Rich1997 <32636319+Rich1997@users.noreply.github.com>
2023-03-02 00:12:16 +0530

    2 files modified

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-    const isGitHubPages =
-        command === 'build' &&
-        window.location.hostname === 'rich1997.github.io';
-    const base = isGitHubPages ? '/CoolFreeFonts/' : '/';
-
-    return {
-        plugins: [react()],
-        base,
-    };
+export default defineConfig({
+    plugins: [react()],
+    base: '/',
 });
