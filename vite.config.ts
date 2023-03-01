d9cfe90ac753214cb114ff23c7631e2273b688e9
Rich1997 <32636319+Rich1997@users.noreply.github.com>
2023-03-02 00:18:11 +0530

    1 file modified

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
+export default defineConfig(() => {
     const isGitHubPages =
-        command === 'build' &&
         process.env.BASE_URL === 'https://rich1997.github.io/CoolFreeFonts/';
 
     return {
