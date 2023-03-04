e04f175570afc9dacaee98ccb312b56f96dcf262
Rich1997 <32636319+Rich1997@users.noreply.github.com>
2023-03-04 13:57:31 +0530

    Clean up tailwind config file

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -11,13 +11,6 @@ module.exports = {
                 'secondary-light': 'var(--secondary-light)',
                 'tertiary-dark': 'var(--tertiary-dark)',
                 'tertiary-light': 'var(--tertiary-light)',
-
-                'primary-dark-rose': 'var(--primary-dark-rose)',
-                'primary-light-rose': 'var(--primary-light-rose)',
-                'secondary-dark-rose': 'var(--secondary-dark-rose)',
-                'secondary-light-rose': 'var(--secondary-light-rose)',
-                'tertiary-dark-rose': 'var(--tertiary-dark-rose)',
-                'tertiary-light-rose': 'var(--tertiary-light-rose)',
             },
         },
     },
